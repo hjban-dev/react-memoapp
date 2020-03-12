6e61cb6c59bcf4fd05699e6f9e85273a801bf7f1
hjban <bananagga@naver.com>
2020-03-12 10:15:28 +0900

    작성기능 구현하기 : post/api/memo

--- server/routes/memo.js
+++ server/routes/memo.js
@@ -5,7 +5,49 @@ import mongoose from "mongoose";
 const router = express.Router();
 
 // WRITE MEMO
-router.post("/", (req, res) => {});
+/* 
+    WRITE MEMO: POST /api/memo
+    BODY SAMPLE: { contents: "sample "}
+    ERROR CODES
+        1: NOT LOGGED IN
+        2: EMPTY CONTENTS
+*/
+router.post("/", (req, res) => {
+	// check login status
+	if (typeof req.session.loginInfo === "undefined") {
+		return res.status(403).json({
+			error: "Not Logged In",
+			code: 1
+		});
+	}
+
+	// check contents valid
+	if (typeof req.body.contents !== "string") {
+		return res.status(400).json({
+			error: "Empty contents",
+			code: 2
+		});
+	}
+
+	if (req.body.contents === "") {
+		return res.status(400).json({
+			errer: "Empty contents",
+			code: 2
+		});
+	}
+
+	// create new memo
+	let memo = new Memo({
+		writer: req.session.loginInfo.username,
+		contents: req.body.contents
+	});
+
+	// save in database
+	memo.save(err => {
+		if (err) throw err;
+		return res.json({ success: true });
+	});
+});
 
 // MODIFY MEMO
 router.put("/:id", (req, res) => {});
